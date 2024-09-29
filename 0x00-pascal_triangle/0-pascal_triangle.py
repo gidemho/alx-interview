80c6ffee2ffd95fddedc165f6b87f0e0ffad9278
gidemho <gidemho@gmail.com>
2024-09-29 02:12:15 +0100

    arggghhhh

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,26 +1,23 @@
-#!/usr/bin/python3
-
 def pascal_triangle(n):
     """
-    Generates a Pascal triangle of height n
+    Generates Pascal's Triangle of height n.
 
     Args:
-        n (int): The height of the triangle
+        n (int): The number of rows in the triangle.
+    
     Returns:
-        triangle: A 2D array representation of the triangle
+        list: A 2D list representing the triangle. Each inner list contains 
+              the elements of the corresponding row in Pascal's Triangle.
     """
     if n <= 0:
         return []
 
     triangle = [[1]]
     for i in range(1, n):
-        left_side = [1]
+        current_row = [1]  # Start each row with 1
         for j in range(1, i):
-            left_side.append(triangle[i - 1][j - 1] + triangle[i - 1][j])
-        left_side.append(1)
-        triangle.append(left_side)
+            current_row.append(triangle[i - 1][j - 1] + triangle[i - 1][j])
+        current_row.append(1)  # End each row with 1
+        triangle.append(current_row)
+    
     return triangle
-
-
-n = int(input("n? "))
-print(pascal_triangle(n))
