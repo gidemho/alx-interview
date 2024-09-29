7cb6a17d240cb6ec4832c9301fac7cf4d6ae8d1a
gidemho <gidemho@gmail.com>
2024-09-29 02:13:39 +0100

    .......

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -14,10 +14,10 @@ def pascal_triangle(n):
 
     triangle = [[1]]
     for i in range(1, n):
-        current_row = [1]  # Start each row with 1
+        current_row = [1]
         for j in range(1, i):
             current_row.append(triangle[i - 1][j - 1] + triangle[i - 1][j])
-        current_row.append(1)  # End each row with 1
+        current_row.append(1)
         triangle.append(current_row)
     
     return triangle
