b15a6fc822471214195d2462cd33fc3ddf4a1e3e
gidemho <gidemho@gmail.com>
2024-09-29 02:16:37 +0100

    linter mods

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,23 +1,25 @@
+#!/usr/bin/python3
+
 def pascal_triangle(n):
     """
-    Generates Pascal's Triangle of height n.
+    Generates a Pascal triangle of height n.
 
     Args:
-        n (int): The number of rows in the triangle.
-    
+        n (int): The height of the triangle.
     Returns:
-        list: A 2D list representing the triangle. Each inner list contains 
-              the elements of the corresponding row in Pascal's Triangle.
+        list: A 2D array representation of the triangle.
     """
     if n <= 0:
         return []
 
     triangle = [[1]]
     for i in range(1, n):
-        current_row = [1]
+        current_row = [1]  # Start each row with 1
         for j in range(1, i):
             current_row.append(triangle[i - 1][j - 1] + triangle[i - 1][j])
-        current_row.append(1)
+        current_row.append(1)  # End each row with 1
         triangle.append(current_row)
     
     return triangle
+
+
